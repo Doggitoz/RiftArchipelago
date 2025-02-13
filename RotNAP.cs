c4d1ac5b0967f37210e1b772331febf4ff29c362
Elly <eric.poore@gmail.com>
2025-02-13 03:28:47 -0600

    Initial Push

---
+++ RotNAP.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace RiftArchipelago;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("RiftOfTheNecroDancer.exe")]
+public class RotNAP : BaseUnityPlugin {
+    internal static new ManualLogSource Logger;
+        
+    private void Awake() {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
