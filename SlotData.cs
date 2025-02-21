9efc6961f2db185ad6a4d0d4ce412dbfeb96b030
Elly <eric.poore@gmail.com>
2025-02-20 23:14:57 -0600

    Added slotdata

--- SlotData.cs
+++ SlotData.cs
@@ -7,9 +7,19 @@ namespace RiftArchipelago {
     public class SlotData {
         public int diamondGoal {get; private set;}
         public string goalSong {get; private set;}
+        public bool deathLink {get; private set;}
+        public string gradeNeeded {get; private set;}
 
         public SlotData(Dictionary<string, object> slotData) {
+            if(slotData.TryGetValue("diamondWinCount", out var diamond_goal)) {
+                diamondGoal = ParseInt(diamond_goal);
+            }
 
+            goalSong = (string) slotData["victoryLocation"];
+        }
+
+        private int ParseInt(object i) {
+            return int.TryParse(i.ToString(), out var result) ? result : -1;
         }
     }
 }
