f88adad3e6e56c37d735fb3392016f8971e626c5
Coleton Wheeler <cpw2965@utulsa.edu>
2025-10-17 14:32:31 -0500

    Squashed commit of the following:
    commit 0358fe3b5b89596ad64fe3d47dba597364f47eb3
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Thu Oct 16 04:15:43 2025 -0500
    
        Full Combo Support
    
        - SlotData field added for full combo requirement
        - Revised logic to fix full combo tracking. Misses are missed monsters, errants are misinputs.
    
    commit ba8ae3c2a61b13f338b50171ce7bb6cd3388fd77
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Wed Oct 15 19:12:12 2025 -0500
    
        Repaired SS Check
    
        - Added custom enum to help with consistency
        - Also fixed misread of S_PLUS in my previous code :3
    
    commit 0f25e07035fb0eca7b16603ec07683e481feb830
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Tue Oct 14 14:07:13 2025 -0500
    
        Unused Private Accessors
    
        - I had a lot of leftover garbage that was unused teehee. It should be a lot more readable now.
    
    commit 74c2de85ac49afd952c0763b7c7905f94542fe59
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Sun Oct 12 02:07:19 2025 -0500
    
        Added grade checking
    
    commit 0ca1fc14ddbfaf84cc6d0c1b24946f42ac44aa5d
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Fri Oct 10 17:52:10 2025 -0500
    
        Reenabled Golden Lute
    
        - oopsie
    
    commit 548c43671eaa31342534bfebba018a9850b4ebc1
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Fri Oct 10 00:38:33 2025 -0500
    
        Indentation + Superfluous code
    
        - Converted to preferred curly bracket formatting
        - Removed some leftover code that wasn't being used anymore
    
    commit 0cddf8d5f540d0d2d2ca1cd8c1fcdd15da20dae0
    Author: Coleton Wheeler <cpw2965@utulsa.edu>
    Date:   Thu Oct 9 21:33:12 2025 -0500
    
        Refactored
    
        - Moved RR logic into a separate class using a different Harmony Postfix method.
        - Left Boss Battle and Mini Game logic untouched.
        - Added logic to send more information to the Archipelago Client: stageDisplayName, levelId, difficulty, letterGrade, isFullCombo, and isRemixMode (some were already being sent).
        - Uses private fields inside of the RhythmRift.RRStageController classes by some weird Harmony helpers and syntax (it works so I just left it)

--- SlotData.cs
+++ SlotData.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using BepInEx.Logging;
 
 namespace RiftArchipelago {
 
@@ -8,10 +7,11 @@ namespace RiftArchipelago {
         public int diamondGoal {get; private set;}
         public string goalSong {get; private set;}
         public bool deathLink {get; private set;}
-        public string gradeNeeded {get; private set;}
+        public Grade gradeNeeded {get; private set;}
         public bool remix {get; private set;}
         public int mgMode {get; private set;}
         public int bbMode {get; private set;}
+        public bool fullComboNeeded {get; private set;}
 
         public SlotData(Dictionary<string, object> slotData) {
             if(slotData.TryGetValue("diamondWinCount", out var diamond_goal)) {
@@ -32,10 +32,53 @@ namespace RiftArchipelago {
             if(slotData.TryGetValue("bossMode", out var boss_mode)) {
                 bbMode = ParseInt(boss_mode);
             }
+            if (slotData.TryGetValue("gradeNeeded", out var grade_needed)) {
+                gradeNeeded = MapObjectToGrade(grade_needed);
+            }
+            if (slotData.TryGetValue("fullComboNeeded", out var fc_needed)) {
+                fullComboNeeded = Convert.ToBoolean(fc_needed);
+            }
         }
 
         private int ParseInt(object i) {
             return int.TryParse(i.ToString(), out var result) ? result : -1;
+        }
+
+        public static Grade MapObjectToGrade(object g) {
+            if (g == null) return Grade.Any;
+            var s = g.ToString().Trim().ToUpper();
+
+            // If it gets sent as an enum value
+            if (int.TryParse(s, out var n)) {
+                return n switch {
+                    0 => Grade.Any,
+                    1 => Grade.C,
+                    2 => Grade.B,
+                    3 => Grade.A,
+                    4 => Grade.S,
+                    5 => Grade.SS,
+                    _ => Grade.Any,
+                };
+            }
+
+            // If it is sent as a string
+            var up = s.ToUpperInvariant();
+            if (up == "S_PLUS") return Grade.SS;
+            else if (up == "SS") return Grade.SS;
+            else if (up == "S") return Grade.S;
+            else if (up == "A") return Grade.A;
+            else if (up == "B") return Grade.B;
+            else if (up == "C") return Grade.C;
+            else return Grade.Any;
+        }
+
+        public enum Grade {
+            Any = 0,
+            C = 1,
+            B = 2,
+            A = 3,
+            S = 4,
+            SS = 5
         }
     }
 }
